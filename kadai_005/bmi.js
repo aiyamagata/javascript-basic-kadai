9eeb0bfb161f144fdd0236668d717660e0f15c85
aiyamagata <tgifai58@gmail.com>
2025-10-29 09:49:51 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,3 +1,10 @@
-let userNumber;
-userNumber = 68/(1.7*1.7);
-console.log(userNumber);
+let userWeight;
+userWeight = 68;
+
+let userHeight;
+userHeight = 1.7;
+
+let userBmi;
+userBmi = userWeight / (userHeight * userHeight);
+
+console.log(userBmi);
